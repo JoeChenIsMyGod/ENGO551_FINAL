afa3fcb83391441be5bca10361175ba4afbcff6f
RonBranch <rjbranch@ucalgary.ca>
2017-04-21 23:56:09 -0600

    Add files via upload

---
+++ wordcloudinput.php
@@ -0,0 +1,19 @@
+<?php
+$host='localhost';
+$db = 'dotamap';
+$username = 'postgres';
+$password = 'admin';
+$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+try{
+// create a PostgreSQL database connection
+$conn = new PDO($dsn);
+// display a message if connected to the PostgreSQL successfully
+
+$region = $_GET("region");
+//determine which table to use
+$meme = $_GET("meme");
+$role = $_GET("role");
+$hero = $_GET("hero");
+$meta = $_GET("meta");
+//user tags, can be empty
+}?>
